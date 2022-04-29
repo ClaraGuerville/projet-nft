d304c75f926f2ec63144766facdc902ed0f49c22
wilsonwesley <wesley-wilson@outlook.com>
2022-04-29 10:32:24 +0200

    Ajustements des controllers et des routes de user

---
+++ controllers/userctrl.js
@@ -0,0 +1,51 @@
+exports.login_get = (req, res)=>{
+	console.log('debugage')
+	res.render('user/login');
+}
+
+exports.login_post = (req, res)=>{
+	User.findOne({
+		pseudo: req.body.pseudo
+	}, (err, user)=>{
+		if (err || !user) {
+			res.redirect('/login');
+			return;
+		}
+
+		bcrypt.compare(req.body.password, user.password, (err, result)=>{
+			if (err || !result) {
+				res.redirect('/login');
+				return;
+			}
+
+			delete user.password;
+			req.session.user = user;
+			res.redirect('/');
+		});
+	});
+}
+
+exports.register_get = (req, res, next)=>{
+	console.log('debugage')
+	res.render('user/register');
+}
+
+exports.register_post = (req, res, next)=>{
+	// 
+    bcrypt.hash(req.body.password, saltRounds, (err, hash)=>{
+		// retrieve the field's values & use it to instantiate a new User. Spread operator : does not take the 'repeat'
+		const user = new User({
+			...req.body,
+			password: hash
+		});
+
+		user.save((err, result)=>{
+			if (err)
+				console.log(err);
+			else
+				console.log(result);
+				res.redirect('/');
+		})
+    });
+
+}
--- routes/user.js
+++ routes/user.js
@@ -14,12 +14,10 @@ const router = express.Router();
 // get the user ctrl
 // const albumctrl = require('./../controllers/userctrl');
 
+// CONTROLLERS
+const userCtrl = require('./../controllers/userctrl');
 
-
-router.get('/register', (req, res, next)=>{
-	console.log('debugage')
-	res.render('user/register');
-});
+router.get('/register', userCtrl.register_get);
 
 /**
  * Get the user model
@@ -27,52 +25,11 @@ router.get('/register', (req, res, next)=>{
  * 
  */
 const User = require('./../models/user');
-router.post('/register', (req, res, next)=>{
-	// 
-    bcrypt.hash(req.body.password, saltRounds, (err, hash)=>{
-		// retrieve the field's values & use it to instantiate a new User. Spread operator : does not take the 'repeat'
-		const user = new User({
-			...req.body,
-			password: hash
-		});
-
-		user.save((err, result)=>{
-			if (err)
-				console.log(err);
-			else
-				console.log(result);
-				res.redirect('/');
-		})
-    });
-
-})
+router.post('/register', userCtrl.register_post);
 
 // route login
-router.get('/login', (req, res)=>{
-	console.log('debugage')
-	res.render('user/login');
-});
+router.get('/login', userCtrl.login_get);
 
-router.post('/login', (req, res)=>{
-	User.findOne({
-		pseudo: req.body.pseudo
-	}, (err, user)=>{
-		if (err || !user) {
-			res.redirect('/login');
-			return;
-		}
-
-		bcrypt.compare(req.body.password, user.password, (err, result)=>{
-			if (err || !result) {
-				res.redirect('/login');
-				return;
-			}
-
-			delete user.password;
-			req.session.user = user;
-			res.redirect('/');
-		});
-	});
-})
+router.post('/login', userCtrl.login_post);
 
 module.exports = router;
